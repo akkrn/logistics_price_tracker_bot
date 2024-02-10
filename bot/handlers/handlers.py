cdeb9c85bfd5f43cc82695bc9bafa2a84145fbc4
Anton Kokorin <ant.kkrn@gmail.com>
2024-02-10 14:40:50 +0700

    change buttons name to -> updated_at
    Signed-off-by: Anton Kokorin <ant.kkrn@gmail.com>

--- bot/handlers/handlers.py
+++ bot/handlers/handlers.py
@@ -208,6 +208,8 @@ async def process_remove_notifications(message: Message):
         for seller in sellers:
             if str(seller.id) in jobs_id:
                 sellers_dict[str(seller.id)] = datetime.datetime.strftime(
+                    seller.updated_at, "%d.%m.%Y %H:%M"
+                ) if seller.updated_at else datetime.datetime.strftime(
                     seller.added_at, "%d.%m.%Y %H:%M"
                 )
         if len(sellers_dict) > 1:
